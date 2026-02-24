bab30157b37f98ce54b198393f4b69daaa6413ad
mehdi-badsi <mehdi.badsi@univ-nantes.fr>
2026-02-24 21:52:39 +0100

    Le code tourne : Il y avait une erreur de signe dans le solveur de Poisson qui était compensé par le mauvais signe dans l'équation d'impulsion sur le solveur FV Remarque générale : Le SL + REMAP est très dissipatif: vraiment plus que le schéma FV...

--- main.jl
+++ main.jl
@@ -5,20 +5,28 @@ using Plots
 using .Threads
 global k = 0.5                #Wave number
 global test_case::String      #test_case =  {landau_damping,two_streams,mono_kinetic}
-global T = 0.1               #Temperature
+global T = 1.0                #Temperature
 global L  = 2π / k            #Size of the domain
-global eps = 0.1              #Debye length
+global eps = 1.0             #Debye length
+global solver::String         #SOLVER = {FV,SL} First Order Implicit AP Finite Volume Schem or Cubic Implicit Semi-Lagragian scheme
 
 function main(hermite_quad)
-    test_case = "mono_kinetic"
-    nx, xmin, xmax = 128, 0.0, L
+    test_case = "landau_damping"
+    solver    = "SL"
+    nx, xmin, xmax = 256, 0.0, L
     mesh_x = UniformMesh(xmin,xmax,nx)
-    nv, vmin, vmax = 128, -6.0, 6.0 
+    nv, vmin, vmax = 256, -6.0, 6.0
     grid_v = UniformGrid(vmin, vmax, nv, T,mesh_x,test_case)
     #Compute the initial condition
     rho, u, rho_tot = compute_initial_condition(mesh_x,grid_v,k,T,test_case)
     phi = zeros(nx+1)
     poisson!(phi, mesh_x, rho_tot, eps)
+    E = -1.0*compute_dx!(phi,mesh_x)
+
+    #For the semi-lagragian scheme
+    x_feet_mesh = zeros(nx+1,nv)
+    rho_pred    = zeros(nx+1,nv)
+    phi_pred    = zeros(nx+1)
 
     #Initialize the streams
     u_at_step_n   = zeros(nx + 1,nv)    
@@ -26,7 +34,7 @@ function main(hermite_quad)
 
     #Set the CFL number and the final time
     dt =  0.1*mesh_x.dx
-    tfinal = 20
+    tfinal = 30
     time = [0.0]
     remap_time = 0.0
 
@@ -38,7 +46,9 @@ function main(hermite_quad)
 
     #Temporal loop
     n = 0
-    while n * dt <= tfinal
+    anim = Animation()
+
+  while n * dt <= tfinal
 	    iter = 0
         err=1e-10
 	    maxiter=50
@@ -51,27 +61,56 @@ function main(hermite_quad)
             remap_time = n * dt
             rho, u = remap_f_on_uniform_grid(mesh_x,grid_v,rho,u)
         end
-        copyto!(rho_at_step_n, rho)
-        copyto!(u_at_step_n, u)	
-        old_u = zeros(nx + 1,nv)
-       #Fixed point loop to solve the non linear MultiStream pressureless Euler-Poisson system
-        while norm(u .- old_u, Inf) / norm(u, Inf) > err && iter < maxiter
-            #Update rho : the streams are packed into groups that are solved on each threads
-            @threads for j in 1:nv	 
-                update_rho!(mesh_x, view(rho, :, j), view(u, :, j), view(rho_at_step_n, :, j), dt)
+        
+        if(solver=="FV")
+            copyto!(rho_at_step_n, rho)
+            copyto!(u_at_step_n, u)	
+            old_u = zeros(nx + 1,nv)
+            #Fixed point loop to solve the non linear MultiStream pressureless Euler-Poisson system
+            while norm(u .- old_u, Inf) / norm(u, Inf) > err && iter < maxiter
+                #Update rho : the streams are packed into groups that are solved on each threads
+                @threads for j in 1:nv	 
+                    update_rho!(mesh_x, view(rho, :, j), view(u, :, j), view(rho_at_step_n, :, j), dt)
+                end
+                #Assemble rho
+	            compute_rho_total!(rho_tot,grid_v,rho)
+                #Solve Poisson
+	            poisson!(phi, mesh_x, rho_tot,eps)	  
+                copyto!(old_u, u)
+                #Update u : the streams are packed into groups that are solved on each threads
+	            @threads for j in 1:nv
+	  	            update_u!(mesh_x, view(rho, :, j), view(u, :, j), phi,
+		            view(rho_at_step_n, :, j), view(u_at_step_n, :, j), dt, maxiter)
+                end
+                iter += 1
+	        end
+        elseif(solver=="SL")
+            copyto!(rho_at_step_n, rho)
+            copyto!(u_at_step_n, u)	
+            @threads for j in 1:nv
+                compute_x_feet_mesh!(dt,mesh_x,view(x_feet_mesh,:,j), view(u_at_step_n, :, j ),E)
+                update_rho_predictor_SL!(mesh_x,view(rho_pred,:,j), view(rho_at_step_n, :, j),view(u_at_step_n, :, j),dt,
+                view(x_feet_mesh, :, j) )
+                #display(rho_pred[:,j])
+            end
+            #Assemble rho
+	        compute_rho_total!(rho_tot,grid_v,rho_pred)
+            #display(rho_tot)
+            #Solve Poisson 
+	        poisson!(phi_pred, mesh_x, rho_tot,eps)
+            #display(phi_pred)
+            E_plus = -1.0.*compute_dx!(phi_pred,mesh_x)
+            #display(E_plus)
+            @threads for j in 1:nv
+                update_u_SL!(mesh_x,E, E_plus, view(u,:,j), view(u_at_step_n,:,j), dt,view(x_feet_mesh,:,j))
+                update_rho_corrector_SL!(mesh_x, view(rho,:,j), view(rho_pred, : ,j), view(u_at_step_n,:,j),view(u,:,j), dt, view(x_feet_mesh,:,j))
             end
             #Assemble rho
 	        compute_rho_total!(rho_tot,grid_v,rho)
-            #Solve Poisson
-	        poisson!(phi, mesh_x, rho_tot,eps)	  
-            copyto!(old_u, u)
-            #Update u : the streams are packed into groups that are solved on each threads
-	        @threads for j in 1:nv
-	  	        update_u!(mesh_x, view(rho, :, j), view(u, :, j), phi,
-		        view(rho_at_step_n, :, j), view(u_at_step_n, :, j), dt, maxiter)
-            end
-         iter += 1
-	    end
+            #Solve Poisson 
+	        poisson!(phi, mesh_x, rho_tot,eps)
+            E = -1.0*compute_dx!(phi,mesh_x)   
+        end
         compute_rho_total!(rho_tot, grid_v, rho)
     
         push!(elec_energy, compute_elec_energy(phi, mesh_x, eps))
@@ -80,8 +119,31 @@ function main(hermite_quad)
         push!(total_energy,compute_elec_energy(phi, mesh_x, eps) + compute_kinetic_energy(rho,u,mesh_x,grid_v))
         n += 1
         push!(time, n * dt)
-        println("iteration: $n , time = $(n * dt), elec energy = $(last(elec_energy)), mass = $(last(mass)),   ||dxU|| = $norm_dx_u")  
+        println("iteration: $n , time = $(n * dt), elec energy = $(last(elec_energy)), mass = $(last(mass)),   ||dxU|| = $norm_dx_u")
+        #Movie of the the solution
+        per = 1000 #Plot every 50 iterations
+        if(mod(n,per) == 1)
+            f_on_grid =  interpolate_f_on_grid(mesh_x,grid_v,rho,u)
+            X = []
+            Y = []
+            Z = []
+            #ZZ = []
+            for i in 1:nx+1
+                for j in 1:nv
+                    X = push!(X,mesh_x.x[i])
+                    Y = push!(Y,grid_v.v[j])
+                    Z = push!(Z,f_on_grid[i,j])
+                    #ZZ = push!(ZZ,f_on_grid[i,j]-exp(-0.5*grid_v.v[j]*grid_v.v[j]/T)/sqrt(2*π*T))
+                end
+            end
+            p = plot(X,Y,Z,st = [:surface],camera = (0,90),xlabel = "x", ylabel="v",)
+            plot!(p; ylims=(-4.,4.))
+            frame(anim)
+        end
+
     end
+    gif(anim, "fig/landau_damping_SL+REMP.gif", fps = 15)
+    
 
     #Plot the final distribution function
     f_on_grid =  interpolate_f_on_grid(mesh_x,grid_v,rho,u)
@@ -100,11 +162,11 @@ function main(hermite_quad)
     plot_f = plot(X,Y,Z,st = [:surface],camera = (0,90),xlabel = "x", ylabel="v")
     plot_df = plot(X,Y,ZZ,st = [:surface],camera = (0,90),xlabel = "x", ylabel="v")
 
-    return time, elec_energy, mass, momentum, total_energy, grid_v, mesh_x, u, rho_tot, plot_f, plot_df
+    return time, elec_energy, mass, momentum, total_energy, grid_v, mesh_x, u, rho_tot, plot_f, plot_df, phi, E
 
 end
-@time time, elec_energy, mass, momentum, total_energy, grid_v, mesh_x, u, rho_tot, plot_f, plot_df = main(true)
-plot(time,elec_energy,yaxis = :log)
+@time time, elec_energy, mass, momentum, total_energy, grid_v, mesh_x, u, rho_tot, plot_f, plot_df,phi,E = main(true)
+plot(time,log.(elec_energy))
 
 #plot(mesh_x.x,u[:,grid_v.nv/2-10:grid_v.nv/2+10], legend = false)
 
