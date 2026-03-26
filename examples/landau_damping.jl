0ff8608a7918df71fddab7188de7e2689b1c73af
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-03-26 17:00:40 +0100

    Update landau_damping.jl
    Add cache for cubic interpolation

--- examples/landau_damping.jl
+++ examples/landau_damping.jl
@@ -6,56 +6,28 @@ using FastInterpolations
 using FFTW
 using Parameters
 using Plots
+using .Threads
 using TimerOutputs
 
 const to = TimerOutput()
 
 struct UniformMesh 
+
+    xmin::Float64
+    xmax::Float64
     nx::Int
     x::Vector{Float64}
     kx::Vector{Float64}
     dx::Float64
-    L::Float64
+
     function UniformMesh(xmin::Float64, xmax::Float64, nx::Int)
-        dx = (xmax - xmin) / (nx + 1)
+        dx = (xmax - xmin) / nx
         x = range(start=xmin, step=dx, length=nx+1)
-        L = (xmax - xmin)
-        kx = collect(2π / (xmax - xmin) * fftfreq(nx + 1, nx + 1))
-        return new(nx, x, kx, dx, L)
+        kx = collect(2π / (xmax - xmin) * fftfreq(nx, nx))
+        return new(xmin, xmax, nx, x, kx, dx)
     end
 
 end
-
-struct UniformGrid 
-    nv::Int
-    v::Vector{Float64}
-    dv::Float64
-    w::Vector{Float64}
-
-    function UniformGrid(vmin, vmax, nv, mesh_x, test_case)
-
-        sf0 = 0.0
-        dv = Float64
-        dv = (vmax - vmin) / (nv)
-        v = LinRange(vmin, vmax, nv)
-        w = zeros(nv)
-        for i in 1:nv
-            sf0 += mean_f0(test_case, mesh_x, v[i]) * dv
-        end
-        for i in 1:nv
-            w[i] = mean_f0(test_case, mesh_x, v[i]) * dv / sf0
-        end
-        return new(nv, v, dv, w)
-    end
-
-end
-
-function compute_dx(v::AbstractVector, mesh::UniformMesh)
-    kx = mesh.kx
-    dx_v = real(ifft(1im * kx .* fft(v)))
-    return dx_v
-end
-
 
 @with_kw struct LandauDamping 
 
@@ -76,76 +48,95 @@ function f0(test_case::LandauDamping, x::Float64, v::Float64)::Float64
 
 end
 
-function mean_f0(test_case::LandauDamping, mesh_x::UniformMesh, v::Float64)::Float64
+function mean_f0(test_case::LandauDamping, mesh::UniformMesh, v::Float64)::Float64
     mf0 = 0.0
-    nx, dx, L = mesh_x.nx, mesh_x.dx, mesh_x.L
+    nx, dx, xmin, xmax = mesh.nx, mesh.dx, mesh.xmin, mesh.xmax
     for i in 1:(nx + 1)
-        x = mesh_x.x[i]
-        mf0 += f0(test_case, x, v) * dx / L
+        x = mesh.x[i]
+        mf0 += f0(test_case, x, v) * dx / (xmax - xmin)
     end
     return mf0
 end
 
-function compute_initial_condition(test_case::LandauDamping, mesh_x::UniformMesh, grid_v::UniformGrid)
+struct UniformGrid 
+
+    nv::Int
+    v::Vector{Float64}
+    dv::Float64
+    w::Vector{Float64}
+
+    function UniformGrid(vmin, vmax, nv, mesh, test_case)
+
+        sf0 = 0.0
+        dv = Float64
+        dv = (vmax - vmin) / (nv)
+        v = LinRange(vmin, vmax, nv)
+        w = zeros(nv)
+        for i in 1:nv
+            sf0 += mean_f0(test_case, mesh, v[i]) * dv
+        end
+        for i in 1:nv
+            w[i] = mean_f0(test_case, mesh, v[i]) * dv / sf0
+        end
+        return new(nv, v, dv, w)
+    end
+
+end
+
+function compute_dx(v::AbstractVector, mesh::UniformMesh)
+    nx = mesh.nx
+    kx = mesh.kx
+    dx_v = real(ifft(1im * kx .* fft(view(v, 1:nx))))
+    push!(dx_v, dx_v[1])
+    return dx_v
+end
+
+
+function compute_initial_condition(test_case::LandauDamping, mesh::UniformMesh, grid::UniformGrid)
 
     k = test_case.k
-    nx, nv = mesh_x.nx, grid_v.nv
+    nx, nv = mesh.nx, grid.nv
     rho = [zeros(nx + 1) for i in 1:nv]
     u = [zeros(nx + 1) for i in 1:nv]
     rho_tot = zeros(nx + 1)
     for j in 1:nv
-        alpha = grid_v.v[j]
-        for i in 1:(nx + 1)
-            x_i = mesh_x.x[i]
-            rho[j][i] = f0(test_case, x_i, alpha) / mean_f0(test_case, mesh_x, alpha)
-            u[j][i] = alpha
-            rho_tot[i] += grid_v.w[j] * rho[j][i]
+        v = grid.v[j]
+        for i in 1:nx
+            x = mesh.x[i]
+            rho[j][i] = f0(test_case, x, v) / mean_f0(test_case, mesh, v)
+            u[j][i] = v
+            rho_tot[i] += grid.w[j] * rho[j][i]
         end
     end
     return rho, u, rho_tot
 end
 
 
-function compute_rho_total!(rho_tot::Vector{Float64}, grid_v::UniformGrid, rho)
+function compute_rho_total!(rho_tot::Vector{Float64}, grid::UniformGrid, rho)
+
     fill!(rho_tot, 0.0)
-
-    for j in eachindex(grid_v.w), i in eachindex(rho_tot)
-        rho_tot[i] += grid_v.w[j] * rho[j][i]
+    for j in eachindex(grid.w), i in eachindex(rho_tot)
+        rho_tot[i] += grid.w[j] * rho[j][i]
     end
+    rho_tot[end] = rho_tot[1]
 end
-
-function compute_norm_dx_u(mesh_x::UniformMesh, grid_v::UniformGrid, u)
-
-    nv = grid_v.nv
-    nx = mesh_x.nx
-    dx = mesh_x.dx
-
-    dx_u = 0.0
-    for l in 1:nv, i in 1:nx
-        du = abs(u[l][i + 1] - u[l][i]) / dx
-        dx_u = max(dx_u, du)
-    end
-
-    return dx_u
-end
-
 
 function poisson!(phi::Vector{Float64}, mesh::UniformMesh, rho_tot::Vector{Float64}; ϵ = 1.0)
 
-    rho_tot_f = fft(rho_tot .- 1)
+    rho_tot_f = fft(rho_tot[1:end-1] .- 1)
     rho_tot_f[1] = 0
     kkx = mesh.kx
     kkx[1] = 1
-    return phi .= +real(ifft((rho_tot_f ./ (kkx .* kkx)))) / (ϵ * ϵ)
+    phi[1:end-1] .= real(ifft((rho_tot_f ./ (kkx .* kkx)))) / (ϵ * ϵ)
+    phi[end] = phi[1]
 
 end
-
 
 function compute_elec_energy(phi::Vector{Float64}, mesh::UniformMesh; ϵ = 1.0)::Float64
     e = 0.0
     nx, dx = mesh.nx, mesh.dx
     for i in eachindex(phi)
-        ir = mod1(i + 1, nx + 1)
+        ir = mod1(i + 1, nx)
         e += 0.5 * ϵ * ϵ * dx * (phi[ir] - phi[i])^2 / (dx * dx)
     end
     return sqrt(e)
@@ -184,9 +175,13 @@ function main(; tfinal = 10)
     rho_at_step_n = zeros(nx + 1)
     u_at_step_n = zeros( nx + 1)
 
-    du_dx = zeros( nx + 1)
     du_dx_plus = zeros( nx + 1)
     e_new = zeros(nx + 1)
+
+    cache = CubicSplineCache(1.0:(nx + 1); bc = PeriodicBC())
+
+    u_hat = zeros(ComplexF64, nx)
+    du_dx = zeros(nx + 1)
 
     while n * dt <= tfinal
 
@@ -194,18 +189,20 @@ function main(; tfinal = 10)
         dx = mesh_x.dx
         fill!(rho_tot, 0.0)
         xi = 1.0:(nx+1)
-        bc = PeriodicBC(endpoint=:exclusive)
+        bc = PeriodicBC()
 
         fill!(rho_tot, 0.0)
 
-        @timeit to "predictor" for j in 1:nv
+
+        for j in 1:nv
             for i in eachindex(e)
                 d = - (dt * u[j][i] - 0.5 * dt * dt * e[i]) / dx
-                xq[i] = mod1(i + d, nx+1)
+                xq[i] = mod1(i + d, nx)
             end
-            du_dx .= compute_dx(u[j], mesh_x)
-            du_dx .= cubic_interp(xi, du_dx, xq, bc = bc)
-            rho_pred .= cubic_interp(xi, rho[j], xq, bc = bc)
+            @timeit to "dx" du_dx .= compute_dx(u[j], mesh_x)
+            du_dx .= cubic_interp(cache, du_dx, xq)
+            rho[j][end] = rho[j][1]
+            rho_pred .= cubic_interp(cache, rho[j], xq)
             rho_tot .+= rho_pred .* grid_v.w[j] .* exp.(-dt .* du_dx)
         end
 
@@ -214,31 +211,35 @@ function main(; tfinal = 10)
             e_pred .= -1.0 .* compute_dx(phi_pred, mesh_x)
         end
 
-        @timeit to "corrector" for j in 1:nv
+        fill!(rho_tot, 0.0)
 
+        for j in 1:nv
+
+            u[j][end] = u[j][1]
             u_at_step_n .= u[j]
             rho_at_step_n .= rho[j]
+
             for i in eachindex(e)
-                b = -0.5 * dt * dt * e[i]
-                d = - (dt * u[j][i] + b) / dx
-                xq[i] = mod1(i + d, nx+1)
+                d = - (dt * u[j][i] -0.5 * dt * dt * e[i]) / dx
+                xq[i] = mod1(i + d, nx)
             end
-            u[j] .= cubic_interp(xi, u_at_step_n, xq, bc = bc)
-            e_new .= cubic_interp(xi, e, xq, bc = bc)
+
+            u[j] .= cubic_interp(cache, u_at_step_n, xq)
+            e_new .= cubic_interp(cache, e, xq)
             e_new .+= e_pred
             u[j] .+= 0.5dt .* e_new
 
-            du_dx = compute_dx(u_at_step_n, mesh_x)
-            du_dx_plus = compute_dx(u[j], mesh_x)
+            @timeit to "dx" du_dx .= compute_dx(u_at_step_n, mesh_x)
+            @timeit to "dx" du_dx_plus .= compute_dx(u[j], mesh_x)
 
-            rho[j] .= cubic_interp(xi, rho_at_step_n, xq, bc = bc)
-            du_dx .= cubic_interp(xi, du_dx, xq, bc = bc)
+            rho[j] .= cubic_interp(cache, rho_at_step_n, xq)
+            du_dx .= cubic_interp(cache, du_dx, xq)
             du_dx .+= du_dx_plus
             rho[j] .*= exp.(-0.5dt .* du_dx)
+            rho_tot .+= rho[j] .* grid_v.w[j] 
         end
 
         @timeit to "poisson" begin
-            compute_rho_total!(rho_tot, grid_v, rho)
             poisson!(phi, mesh_x, rho_tot)
             e .= -1.0 * compute_dx(phi, mesh_x)
         end
@@ -254,6 +255,8 @@ function main(; tfinal = 10)
     return time, elec_energy
 
 end
+
+@show nthreads()
 
 @time time, elec_energy = main( tfinal = 50.0 )
 
