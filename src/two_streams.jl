1dcd7914fd545d68d0d3077c98f7d5a570be6144
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-03-27 21:55:38 +0100

    Update two_streams.jl

--- src/two_streams.jl
+++ src/two_streams.jl
@@ -29,3 +29,50 @@ where:
 
 end
 
+function f0(x::Float64, v::Float64, k::Float64, T::Float64, u0::Float64)::Float64
+    a = 0.01
+    f0 =
+        (1.0 / sqrt(2π*T)) *
+        (0.5 * exp(-0.5 * (v-u0) * (v-u0)/T) + 0.5 * exp(-0.5 * (v+u0) * (v+u0)/T)) *
+        (1 + a * cos(k * x))
+    return f0
+end
+
+function mean_f0(test_case::TwoStreams, mesh_x::AbstractMesh, v::Float64)::Float64
+    mf0 = 0.0
+    nx, dx, xmin, xmax = mesh_x.nx, mesh_x.dx, mesh_x.xmin, mesh_x.xmax
+    k = 2pi/(xmax - xmin)
+    T = test_case.vth
+    u0 = test_case.v0
+    for i = 1:nx
+        x = mesh_x.x[i]
+        mf0 += (f0(x, v, k, T, u0) * dx)/(xmax-xmin)
+    end
+    return mf0
+end
+
+function compute_initial_condition(
+    mesh_x::AbstractMesh,
+    grid_v::AbstractGrid,
+    k::Float64,
+    T::Float64,
+    test_case::TwoStreams,
+)
+
+    nx, nv = mesh_x.nx, grid_v.nv
+    rho = zeros(nx, nv)
+    u = zeros(nx, nv)
+    for j = 1:nv
+        alpha = grid_v.v[j]
+        for i = 1:nx
+            x_i = mesh_x.x[i]
+            rho[i, j] =
+                f0(x_i, alpha, k, T, test_case.v0) /
+                mean_f0(test_case, mesh_x, alpha)
+            u[i, j] = alpha
+        end
+    end
+    return rho, u
+end
+
+
