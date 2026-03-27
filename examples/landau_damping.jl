ed7078f97a8d583a067593f640b86eea3c0266a4
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-03-27 08:39:58 +0100

    Update landau_damping.jl

--- examples/landau_damping.jl
+++ examples/landau_damping.jl
@@ -4,6 +4,7 @@
 using DispersionRelations
 using FastInterpolations
 using FFTW
+using LinearAlgebra
 using Parameters
 using Plots
 using .Threads
@@ -29,7 +30,9 @@ struct UniformMesh
 
 end
 
-@with_kw struct LandauDamping 
+abstract type InitialCondition end
+
+@with_kw struct LandauDamping <: InitialCondition
 
     α::Float64 = 0.001
     k::Float64 = 0.5
@@ -48,7 +51,8 @@ function f0(test_case::LandauDamping, x::Float64, v::Float64)::Float64
 
 end
 
-function mean_f0(test_case::LandauDamping, mesh::UniformMesh, v::Float64)::Float64
+
+function mean_f0(test_case::InitialCondition, mesh::UniformMesh, v::Float64)::Float64
     mf0 = 0.0
     nx, dx, xmin, xmax = mesh.nx, mesh.dx, mesh.xmin, mesh.xmax
     for i in 1:nx
@@ -91,34 +95,25 @@ function compute_dx(v::AbstractVector, mesh::UniformMesh)
 end
 
 
-function compute_initial_condition(test_case::LandauDamping, mesh::UniformMesh, grid::UniformGrid)
+function compute_initial_condition(test_case::InitialCondition, mesh::UniformMesh, grid::UniformGrid)
 
     k = test_case.k
     nx, nv = mesh.nx, grid.nv
-    rho = [zeros(nx) for i in 1:nv]
+    rho = zeros(nx, nv)
     u = zeros(nx, nv)
     rho_tot = zeros(nx)
     for j in 1:nv
         v = grid.v[j]
         for i in 1:nx
             x = mesh.x[i]
-            rho[j][i] = f0(test_case, x, v) / mean_f0(test_case, mesh, v)
+            rho[i,j] = f0(test_case, x, v) / mean_f0(test_case, mesh, v)
             u[i,j] = v
-            rho_tot[i] += grid.w[j] * rho[j][i]
+            rho_tot[i] += grid.w[j] * rho[i,j]
         end
     end
     return rho, u, rho_tot
 end
 
-
-function compute_rho_total!(rho_tot::Vector{Float64}, grid::UniformGrid, rho)
-
-    fill!(rho_tot, 0.0)
-    for j in eachindex(grid.w), i in eachindex(rho_tot)
-        rho_tot[i] += grid.w[j] * rho[j][i]
-    end
-    rho_tot[end] = rho_tot[1]
-end
 
 function poisson!(phi::Vector{Float64}, mesh::UniformMesh, rho_tot::Vector{Float64}; ϵ = 1.0)
 
@@ -140,15 +135,16 @@ function compute_elec_energy(phi::Vector{Float64}, mesh::UniformMesh; ϵ = 1.0):
     return sqrt(e)
 end
 
-function main(; tfinal = 10)
+
+function main(; tfinal = 40)
 
     k = 0.5
     α = 0.001
     test_case = LandauDamping(α = α, k = k)
-
     nx, xmin, xmax = 128, 0.0, 2π / k
-    mesh_x = UniformMesh(xmin, xmax, nx)
     nv, vmin, vmax = 256, -6.0, 6.0
+    
+    mesh_x = UniformMesh(xmin, xmax, nx)
     grid_v = UniformGrid(vmin, vmax, nv, mesh_x, test_case)
 
     rho, u, rho_tot = compute_initial_condition(test_case, mesh_x, grid_v)
@@ -167,98 +163,106 @@ function main(; tfinal = 10)
 
     e_pred = zeros(nx)
     phi_pred = zeros(nx)
-    rho_pred = zeros(nx)
+    rho_pred = zeros(nx, nv)
 
     xi = 1.0:nx
-    xq = zeros(nx)
-    rho_at_step_n = zeros(nx)
-    u_at_step_n = zeros(nx)
-
-    du_dx_plus = zeros(nx)
+    xq = [zeros(nx) for i in 1:nv]
     e_new = zeros(nx)
+    du_dx = zeros(nx)
 
     bc = PeriodicBC(endpoint = :exclusive)
-    du_dx = zeros(nx)
 
     v = zeros(nx)
     dv = zeros(nx, nv)
+    dv_plus = zeros(nx, nv)
     v_hat = zeros(ComplexF64, nx, nv)
 
-    while n * dt <= tfinal
+    dx = mesh_x.dx
 
-        nx = mesh_x.nx
-        dx = mesh_x.dx
+    t = 0.0
+    @show nstep = floor(Int, tfinal / dt) + 1
+
+    for istep in 1:nstep
+
+        dt = min(dt, tfinal-t)
+
+        for j in 1:nv, i in eachindex(e)
+            d = - (dt * u[i,j] -0.5 * dt * dt * e[i]) / dx
+            xq[j][i] = mod1(i + d, nx)
+        end
+
+
+        @timeit to "fft" begin
+            v_hat .= fft(u, 1)
+            v_hat .*= 1im .* mesh_x.kx
+            dv .= real(ifft(v_hat, 1))
+        end
+
         fill!(rho_tot, 0.0)
-        xi = 1.0:nx
 
-        fill!(rho_tot, 0.0)
-
-        v_hat .= fft(u, 1)
-        v_hat .*= 1im .* mesh_x.kx
-        v .= real(ifft(v_hat, 1))
-
-        for j in 1:nv
-            for i in eachindex(e)
-                d = - (dt * u[i,j] - 0.5 * dt * dt * e[i]) / dx
-                xq[i] = mod1(i + d, nx)
-            end
-            v .= cubic_interp(xi, view(dv, :, j), xq, bc = bc)
-            rho_pred .= cubic_interp(xi, rho[j], xq, bc = bc)
-            rho_tot .+= rho_pred .* grid_v.w[j] .* exp.(-dt .* v)
+        @timeit to "predictor" for j in 1:nv
+            v .= cubic_interp(xi, view(dv, :, j), xq[j], bc = bc)
+            rho_pred[:, j] .= cubic_interp(xi, view(rho,:,j), xq[j], bc = bc)
+            rho_pred[:, j] .*= exp.(-dt .* v)
         end
 
         @timeit to "poisson" begin
+            rho_tot .= vec(sum(rho_pred .* grid_v.w', dims=2))
             poisson!(phi_pred, mesh_x, rho_tot)
             e_pred .= -1.0 .* compute_dx(phi_pred, mesh_x)
         end
 
-        fill!(rho_tot, 0.0)
 
-        for j in 1:nv
+        @timeit to "fft" begin
+            v_hat .= fft(u, 1)
+            v_hat .*= 1im .* mesh_x.kx
+            dv .= real(ifft(v_hat, 1))
+        end
 
-            u_at_step_n .= u[:, j]
-            rho_at_step_n .= rho[j]
+        @timeit to "corrector" for j in 1:nv
 
-            for i in eachindex(e)
-                d = - (dt * u[i,j] -0.5 * dt * dt * e[i]) / dx
-                xq[i] = mod1(i + d, nx)
-            end
-
-            u[:, j] .= cubic_interp(xi, u_at_step_n, xq, bc = bc)
-            e_new .= cubic_interp(xi, e, xq, bc = bc)
+            u[:, j] .= cubic_interp(xi, view(u, :, j), xq[j], bc = bc)
+            e_new .= cubic_interp(xi, e, xq[j], bc = bc)
             e_new .+= e_pred
             u[:, j] .+= 0.5dt .* e_new
 
-            @timeit to "dx" du_dx .= compute_dx(u_at_step_n, mesh_x)
-            @timeit to "dx" du_dx_plus .= compute_dx(u[:, j], mesh_x)
+        end
 
-            rho[j] .= cubic_interp(xi, rho_at_step_n, xq, bc = bc)
-            du_dx .= cubic_interp(xi, du_dx, xq, bc = bc)
-            du_dx .+= du_dx_plus
-            rho[j] .*= exp.(-0.5dt .* du_dx)
-            rho_tot .+= rho[j] .* grid_v.w[j] 
+        @timeit to "fft" begin
+            v_hat .= fft(u, 1)
+            v_hat .*= 1im .* mesh_x.kx
+            dv_plus .= real(ifft(v_hat, 1))
+        end
+
+        fill!(rho_tot, 0.0)
+
+        @timeit to "corrector" for j in 1:nv
+
+            rho[:,j] .= cubic_interp(xi, view(rho, :, j), xq[j], bc = bc)
+            du_dx .= cubic_interp(xi, view(dv, :, j), xq[j], bc = bc)
+            du_dx .+= view(dv_plus, :, j)
+            rho[:,j] .*= exp.(-0.5dt .* du_dx)
+
         end
 
         @timeit to "poisson" begin
+            rho_tot .= vec(sum(rho .* grid_v.w', dims=2))
             poisson!(phi, mesh_x, rho_tot)
             e .= -1.0 * compute_dx(phi, mesh_x)
         end
 
-
         push!(elec_energy, compute_elec_energy(phi, mesh_x))
-        n += 1
-        push!(time, n * dt)
-        #println("iteration: $n , time = $(n * dt), elec energy = $(last(elec_energy)), mass = $(last(mass)),   ||dxU|| = $norm_dx_u")
+        t += dt
+        push!(time, t)
+        println("iteration: $istep , time = $(t), elec energy = $(last(elec_energy))")
 
-    end
+    end  
 
     return time, elec_energy
 
 end
 
-@show nthreads()
-
-@time time, elec_energy = main( tfinal = 50.0 )
+@time time, elec_energy = main( tfinal = 100.0 )
 
 show(to)
 
