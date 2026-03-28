5a064bd06a52084867b0ee6b1577abc843a4d72e
Pierre Navaro <pierre.navaro@math.cnrs.fr>
2026-03-28 10:44:04 +0100

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -1,30 +1,73 @@
-using Documenter
 using MultiPhaseVlasov
+using Documenter
 using Plots
 
 ENV["GKSwstype"] = "100"
 
-makedocs(
-    sitename = "MultiPhaseVlasov.jl",
-    authors = "Mehdi Badsi and Pierre Navaro",
-    modules = [MultiPhaseVlasov],
-    format = Documenter.HTML(;
-        prettyurls = get(ENV, "CI", nothing) == "true",
-        mathengine = MathJax(
-            Dict(
-                :TeX =>
-                    Dict(:equationNumbers => Dict(:autoNumber => "AMS"), :Macros => Dict()),
-            ),
-        ),
-    ),
-    doctest = false,
-    pages = [
-        "Documentation" => "index.md",
-    ],
-    remotes = nothing
+DocMeta.setdocmeta!(MultiPhaseVlasov, :DocTestSetup, :(using MultiPhaseVlasov); recursive = true)
+
+# Add titles of sections and overrides page titles
+const titles = Dict(
+    # "10-tutorials" => "Tutorials", # example folder title
+    "91-developer.md" => "Developer docs",
 )
 
-deploydocs(
-    devbranch = "main",
-    repo = "github.com/juliavlasov/MultiPhaseVlasov.jl.git",
+function recursively_list_pages(folder; path_prefix = "")
+    pages_list = Any[]
+    for file in readdir(folder)
+        if file == "index.md"
+            # We add index.md separately to make sure it is the first in the list
+            continue
+        end
+        # this is the relative path according to our prefix, not @__DIR__, i.e., relative to `src`
+        relpath = joinpath(path_prefix, file)
+        # full path of the file
+        fullpath = joinpath(folder, relpath)
+
+        if isdir(fullpath)
+            # If this is a folder, enter the recursion case
+            subsection = recursively_list_pages(fullpath; path_prefix = relpath)
+
+            # Ignore empty folders
+            if length(subsection) > 0
+                title = if haskey(titles, relpath)
+                    titles[relpath]
+                else
+                    @error "Bad usage: '$relpath' does not have a title set. Fix in 'docs/make.jl'"
+                    relpath
+                end
+                push!(pages_list, title => subsection)
+            end
+
+            continue
+        end
+
+        if splitext(file)[2] != ".md" # non .md files are ignored
+            continue
+        elseif haskey(titles, relpath) # case 'title => path'
+            push!(pages_list, titles[relpath] => relpath)
+        else # case 'title'
+            push!(pages_list, relpath)
+        end
+    end
+
+    return pages_list
+end
+
+function list_pages()
+    root_dir = joinpath(@__DIR__, "src")
+    pages_list = recursively_list_pages(root_dir)
+
+    return ["index.md"; pages_list]
+end
+
+makedocs(;
+    modules = [MultiPhaseVlasov],
+    authors = "Mehdi Badsi <mehdi.badsi@univ-nantes.fr>, Pierre Navaro <perre.navaro@math.cnrs.fr>, Nicolas Crouseilles <nicolas.crouseilles@inria.fr> ",
+    repo = "https://github.com/juliavlasov/MultiPhaseVlasov.jl/blob/{commit}{path}#{line}",
+    sitename = "MultiPhaseVlasov.jl",
+    format = Documenter.HTML(; canonical = "https://juliavlasov.github.io/MultiPhaseVlasov.jl"),
+    pages = list_pages(),
 )
+
+deploydocs(; repo = "github.com/juliavlasov/MultiPhaseVlasov.jl")
